73d546844d2b1d10d0d17d03329d1a05044a868d
Andrew Schulz <anschulz@scu.edu>
2021-11-13 22:29:30 -0800

    Now includes dangerous slowdowns for incidents

--- incident.js
+++ incident.js
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 
-var token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6Imp6eDV4NzFtYTQiLCJ0b2tlbiI6eyJpdiI6IjU2NWMxZWNkOTRjMGVhNGQwNDgwYTBmYjI5ZWFkYjg5IiwiY29udGVudCI6ImFmYWIyOTExNzM2N2IyYzI1ZjZhOTY3NzUxMGI5ZTIxZDM4OTJkMzhkZDE0MjZjYzI0YmJiZWFmYmRjZmQ4Njk4MDhhYWFjNzZmNTg3Y2MyZjFjNDlkNTZjOGYyN2IwYzk4MTgxZmUzNDA0MDI1ODhhYzY3YjhhYTU2ODIxY2QwODA5YWI2ZjBjMTFiN2ZlODkwOGVkNzNhMWEwYjc5MzgxMTViOThkYzg0NjNjMWU3YTRiMGNhZDY2MDI5NDgxYWIyZjcxMTVjZDIwYTA4MTc0YjVjZGRmOWM3NzQyMDg0YTZkMDkyNTIzYWY3MmZlZTBiNTk0ODUwODBhZmFhZjZmZTk2ZjdlZGI2YTI0YjUxYmUxNDgwMzA4YzE3N2U5NmRmMWJiNGY5OGNlM2RiMDg1NmQ2N2RhZTYyZTc4ZmFlYjRlYWQ3NGVmNmFlNTljNGY3MWQyNDJhZjZmOWJiOTMxOWJlOWVlZWU4YzUxMjA3N2JjYzU0ODllMjIyNTE5MzQ3YjM4MTRmNWM0MTc4NTc4NWVlNmE3YmM5YTY1ODM5YWQ2OTRkM2ExNGU0ZTJmZTA2YTFlMWMwZDE3MGZjNjFhYWUyNmZkY2I4ZjYzNzllOWVkMjU5YmZiNGRlN2E5YzNiNjA0NTIxZGMyYzBmNzM3NTA3ZDdlZGFlYmEzMjEzNzc4ODUyYjJlZDRkZDc4MTQwN2MwZmJkYTJlNGQ1MWQyZDkxYzgwODJkYmMyM2VmMTE5NDc0ZWJiMDQxZmU0OTFkMWFkM2NlNGFkNjExNjJkMjM0MjgwNzQ0OTI5ZTk2ZWNjMWNmNzFmODVjYWUxMTRjNWJlZmVhMGIyOGJhNTRmNGI2MTc3MmEyOTJlM2ZlYTY2MTdjIn0sInNlY3VyaXR5VG9rZW4iOnsiaXYiOiI1NjVjMWVjZDk0YzBlYTRkMDQ4MGEwZmIyOWVhZGI4OSIsImNvbnRlbnQiOiI4MDhhMmUzZDQ1NzdlOTgxNTg3MGVmN2E2YjNhYjQyOWVjYTYzNzNlYTQwOTBlYTEzMjhmOWFjOTg1YzZjZTA5ZWY5ZGExZDQyYTc3NmFmY2UwY2JhMjY4In0sImp0aSI6IjBkZjgxZTAzLTY2OWYtNDdkNi1iM2E3LWJjMGJhYjE0Njc1MSIsImlhdCI6MTYzNjg2NDAzNiwiZXhwIjoxNjM2ODY3NjM2fQ.obcZdKnDqlvv6FDNpLIVJNCzWWh-Q8kI_K9NnIm8E6s"
+var token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6ImxwejljeTRsbjYiLCJ0b2tlbiI6eyJpdiI6ImJiYWYyMzQ0MDkwNGViZmEwOTZhYmQ3NzhlNjI0YWY5IiwiY29udGVudCI6ImNkNmJhOTQwMmNmYWVmNWQ5NzAyY2UyOTBhY2FiMmQyZTlmZDUzOTdiMDgxMzAzNTEwMDBkYzAwODBhZGM0ODExOTI5ZWIwNzBkNGYyMDYzYzg1ODA1ZGI0MThjOTIwZThjMjdlMWUzZGJiZTZkMzZmZmE1MDY4MGYzNDMxNjkyNGU1NWIyMTJmODRkNzdiMDVjZDliMjFkNjMwMzNiYzU5ZDgxNmQ1YTMwYjAyYzY2NjMzZGZmZWVlOTg4ODVkNzhlYmM5MzUyZjIzMWE3YmI3ZTNlNDIwNjZjMjIzYWM1MzIxZjIzM2VlZjJjN2U4OGQ1MGRlOThhZjJhOTIxODI4MDM5MjEyZTNjNTc2OGZlNTNlOTUyMGQxOTc5OThiYmMwZWNhNjU2Y2RjMTBkNzYzZmE2NDliOGY1Yjk3MTZkMDUzYjg4YTQxNjUxMTJkNmQzZDg3ZWIzOGYyN2ZlZWEwOGUwNDM4OTNiNjAxNDI4MjU2ZDc3ZjUzMjQ0YzE2ZmY2MDY0MDE4N2E2MDlkNmJiN2IwZDRkZmM1ZTRmMzNiZTg5MDNlNDc5MjA2NzVjZDE1NzlmMWIyMjJkN2ZjZDEwYzlmMzcwN2FkMGIyYTc2NjM5ZDNlNmRlMmQ2ODAyNmQ4Mzg0OTM0MGZhMTZmZWEzZDFmOTA1NGRkODRhOTQyOTI3NWRmOWM2MmM4MDU2OTAyMTQwNzBiYjA5ODIxOTM1MTRjOGVhZjEwMmIwODY0ZmYwMGU3NzhkZWE1NGNlYjNjMDExZDU0ZDhiN2FkN2NiZjhlMzc5NGY0ZDBmMWUxMWFiNTViY2MxZjIwNGQzYzc5ZGQyMGFhNTMwZjk1YTM1MWRlOGYyY2MxM2NmMzgxNjRiOWZkIn0sInNlY3VyaXR5VG9rZW4iOnsiaXYiOiJiYmFmMjM0NDA5MDRlYmZhMDk2YWJkNzc4ZTYyNGFmOSIsImNvbnRlbnQiOiJlZTUwZGE2NTNhYzliYzY1ZTgwNmFlNTM2OWRlY2Y5MWNjZGI2MDgyOWY4YjE3MzkxZjM1YTA0NzhkYjc4NGY0MjMzZGY1MTQ1NzZlMGE0NDk5NmYwY2U1In0sImp0aSI6IjZhMWI2ZWM5LTdkYzYtNGQ3Zi05MTk1LWRhM2Y3ZDIyMTEwMiIsImlhdCI6MTYzNjg2NzkxNSwiZXhwIjoxNjM2ODcxNTE1fQ.Z33YgxyrXaA54s0Q0oilvQDL6t0dJZULcUIKeiRHzls"
 var urlincident = "https://api.iq.inrix.com/v1/incidents?box=37.81154996811045%7C-122.53116454369045%2C37.709691538514235%7C-122.34959352631057&incidentoutputfields=All&incidenttype=Incidents&locale=en"
 var urlroute = "https://api.iq.inrix.com/findRoute?wp_1=37.771305%2C-122.424255&wp_2=37.778340%2C-122.423349&maxAlternates=2&routeOutputFields=p&format=json"
 var urlslow = "https://api.iq.inrix.com/v1/dangerousSlowdowns?box=37.81154996811045%7C-122.53116454369045%2C37.709691538514235%7C-122.34959352631057&units=1"
@@ -45,9 +45,19 @@ var incArray = []; //incident array
 for (let i = 0; i < itemInc.result.incidents.length; ++i) {
     incArray.push(itemInc.result.incidents[i]);
 }
-//console.log(incArray[2].parameterizedDescription.roadName);
-//console.log(itemRoute.result.trip.routes[0].points.coordinates);
-// console.log(itemRoute.result.trip.routes[0].summary.roads.name);
+
+var sevArray = []; //severity of incident
+var totArray = []; //incident array with dangerous slow downs
+
+for (let i = 0; i < itemInc.result.incidents.length; ++i) {
+    totArray.push(itemInc.result.incidents[i]);
+    sevArray.push(itemInc.result.incidents[i].severity)
+}
+for (let i = 0; i < Dangslow.result.dangerousSlowdowns.length; ++i) {
+    totArray.push(Dangslow.result.dangerousSlowdowns[i].location.geometry.coordinates);
+    sevArray.push(Dangslow.result.dangerousSlowdowns[i].severity);
+}
+
 function incident_on_route() {
     var incCounter = [itemRoute.result.trip.routes.length];
     for (let init = 0; init < itemRoute.result.trip.routes.length; ++init) {
@@ -57,17 +67,17 @@ function incident_on_route() {
    // go through all coordinate points in route and compare to incident coords
     for(let i = 0; i < itemRoute.result.trip.routes.length; ++i) // iterate through the routes
     {
-        for(let j = 0; j < itemInc.result.incidents.length; ++j) 
+        for(let j = 0; j < totArray.length; ++j) 
         {    
             for(let k = 0; k < itemRoute.result.trip.routes[i].points.coordinates.length; ++k)
             { 
-                console.log("Difference: ", Math.abs(incArray[j].geometry.coordinates[0]-itemRoute.result.trip.routes[i].points.coordinates[k][0]));
+                //console.log("Difference: ", Math.abs(incArray[j].geometry.coordinates[0]-itemRoute.result.trip.routes[i].points.coordinates[k][0]));
                 // Comparing the Coordinates of the Incident and the points on the route
-                if((Math.abs(itemRoute.result.trip.routes[i].points.coordinates[k][1] - incArray[j].geometry.coordinates[1])) <= 0.001 
-                && Math.abs(incArray[j].geometry.coordinates[0]-itemRoute.result.trip.routes[i].points.coordinates[k][0]) <= 0.001
+                if((Math.abs(itemRoute.result.trip.routes[i].points.coordinates[k][1] - totArray[j][1])) <= 0.001 
+                && Math.abs(totArray[j][0]-itemRoute.result.trip.routes[i].points.coordinates[k][0]) <= 0.001
                 ){
                     
-                    incCounter[i]+= incArray[j].severity;
+                    incCounter[i] += sevArray[j];
                     break;
                 }
             }
@@ -97,5 +107,3 @@ function avoidance() {
 }
 
 //avoidance();
-console.log(itemRoute.result.trip.routes[0].points.coordinates.length);
-//console.log(itemInc.result.incidents)
