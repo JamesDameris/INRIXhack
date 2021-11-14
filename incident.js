2f0182c24c292603fc2202fa1d19ba5f5e0edf54
jamesdameris <jamesdameris@gmail.com>
2021-11-13 20:25:21 -0800

    forloop problems

--- incident.js
+++ incident.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 var token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6Imp6eDV4NzFtYTQiLCJ0b2tlbiI6eyJpdiI6IjU2NWMxZWNkOTRjMGVhNGQwNDgwYTBmYjI5ZWFkYjg5IiwiY29udGVudCI6ImFmYWIyOTExNzM2N2IyYzI1ZjZhOTY3NzUxMGI5ZTIxZDM4OTJkMzhkZDE0MjZjYzI0YmJiZWFmYmRjZmQ4Njk4MDhhYWFjNzZmNTg3Y2MyZjFjNDlkNTZjOGYyN2IwYzk4MTgxZmUzNDA0MDI1ODhhYzY3YjhhYTU2ODIxY2QwODA5YWI2ZjBjMTFiN2ZlODkwOGVkNzNhMWEwYjc5MzgxMTViOThkYzg0NjNjMWU3YTRiMGNhZDY2MDI5NDgxYWIyZjcxMTVjZDIwYTA4MTc0YjVjZGRmOWM3NzQyMDg0YTZkMDkyNTIzYWY3MmZlZTBiNTk0ODUwODBhZmFhZjZmZTk2ZjdlZGI2YTI0YjUxYmUxNDgwMzA4YzE3N2U5NmRmMWJiNGU1OGNlM2NiM2U1NmQ2NmRiZTYyZTdiMTg0ZmNlYmM3NDJiY2FkNjNjNGY3MWQyNDJhZjZmOWJiOTMxOWJlOWVlZWU4YzUxMjA3N2JjYzQ0YWJhODIyNmY4MzQ3YjM4MTRmNWM0MTc4NTc4NWVlNmE3YmM5YTY1ODM5YWQ2OTRkM2ExNGU0ZTJmZTA2YTFlMWMwZDE3MGZjNjFhYWUyNmZkY2I4ZjYzNzllOWVkMjU5YmZiNGRlN2E5YzNiNjA0NTIxZGMyYzBmNzM3NTA3ZDdlZGFlYmEzMjEzNzc4ODUyYjJlZDRkZDc4MTQwN2MwZmJkYTJlNGQ1MWQyZDkxYzgwODJkYmMyM2VmMTE5NDc0ZWJiMDQxZmU0OTRiMzJiMWE5MjJkNTJhMGJlMDM1MzA0NTA2YTNhMzg1Y2ZjOGYyMWNhODYyYTQyYzQxNWVkOGFhNTUzZGViNjllNGM3M2I0OWUxOWZjODliYWM3ZjdjIn0sInNlY3VyaXR5VG9rZW4iOnsiaXYiOiI1NjVjMWVjZDk0YzBlYTRkMDQ4MGEwZmIyOWVhZGI4OSIsImNvbnRlbnQiOiJhMjg0MDg0YzVhMTc5NmU2Mjk2NWY2N2E3NDEwZWUyNGFhODgyYzI0YzUxOTBjY2E3NWJhOThkZmEwY2NlNjQ0YjVmYWFmOWIyYjZhNDJkYWZlZTBhMjY4In0sImp0aSI6IjRlOWNhMWQxLWZlYTgtNGNmYS05MzA5LWNmN2I3NWJjZGEyMCIsImlhdCI6MTYzNjg2MDkwNCwiZXhwIjoxNjM2ODY0NTAyfQ.qfQFzCMoIgdbrMK2ZSP2NP-EyuT_zry5wJEibs3waPA"
 var urlincident = "https://api.iq.inrix.com/v1/incidents?box=37.810716%7C%20-122.457432%2C37.767597%7C-122.380071&incidentoutputfields=All&incidenttype=Flow,RoadWeather&locale=en"
-var urlroute = "https://na-api.inrix.com/Traffic/Inrix.ashx?Action=FindRoute&units=1&locale=en-US&wp_1=37.766223,-122.426577&wp_2=37.763183,-122.421556&maxAlternates=2&RouteOutputFields=B,M,P,S,W&RoutingType=Traffic&IsAmbiguousOrigin=true&format=json&token=NhOHgUOddmZtbQmBW844ylAJ7vGhPxw-hcssnMhOMW8|&UseTraffic=true&compress=true"
+var urlroute = "https://api.iq.inrix.com/findRoute?wp_1=37.771305%2C-122.424255&wp_2=37.778340%2C-122.423349&maxAlternates=2&routeOutputFields=p&format=json"
 async function test() {
     const res = await fetch(urlincident, {
         headers: {
@@ -30,7 +30,7 @@ for (let i = 0; i < itemInc.result.incidents.length; ++i) {
     incArray.push(itemInc.result.incidents[i]);
 }
 //console.log(incArray[2].parameterizedDescription.roadName);
-console.log(itemRoute.result.trip.routes[0].points.coordinates);
+//console.log(itemRoute.result.trip.routes[0].points.coordinates);
 // console.log(itemRoute.result.trip.routes[0].summary.roads.name);
 function incident_on_route() {
     var incCounter = [itemRoute.result.trip.routes.length];
@@ -41,16 +41,19 @@ function incident_on_route() {
    // go through all coordinate points in route and compare to incident coords
     for(let i = 0; i < itemRoute.result.trip.routes.length; ++i)
     {
-        for(let k = 0; k < itemRoute.result.trip.routes[i].points.coordinates.length; ++k)
+        for(let j = 0; j < itemInc.result.incidents.length; ++j)
         {
-            for(let j = 0; j < itemInc.result.incidents.length; ++j)
-            {
-                if( Math.abs(itemRoute.result.trip.routes[i].points.coordinates[k] - incArray[j].geometry.coordinates) > 0.001 ) {
+            for(let k = 0; k < itemRoute.result.trip.routes[i].points.coordinates.length; ++k)
+            {    
+            console.log("Difference: ", Math.abs(incArray[j].geometry.coordinates[0]-itemRoute.result.trip.routes[i].points.coordinates[k][0]));
+                if((Math.abs(itemRoute.result.trip.routes[i].points.coordinates[k][1] - incArray[j].geometry.coordinates[1])) <= 0.001 
+                && Math.abs(incArray[j].geometry.coordinates[0]-itemRoute.result.trip.routes[i].points.coordinates[k][0]) <= 0.001
+                ){
                     ++incCounter[i];
+                    break;
                 }
             }
-        }
-        
+        }  
     }
     return incCounter;
     //if coordinates are within certain range(will not be exact coords)
@@ -75,4 +78,4 @@ function avoidance() {
 }
 
 //avoidance();
-//console.log(itemRoute.result.trip.routes[0].points.coordinates.length);
+console.log(itemRoute.result.trip.routes[0].points.coordinates.length);
