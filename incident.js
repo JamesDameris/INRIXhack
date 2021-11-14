a15ce60a2a0d0b711678a056a72e6378d904b62b
jamesdameris <jamesdameris@gmail.com>
2021-11-13 23:15:25 -0800

    DONE, for now . . .

--- incident.js
+++ incident.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
-var token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6ImxwejljeTRsbjYiLCJ0b2tlbiI6eyJpdiI6ImJiYWYyMzQ0MDkwNGViZmEwOTZhYmQ3NzhlNjI0YWY5IiwiY29udGVudCI6ImNkNmJhOTQwMmNmYWVmNWQ5NzAyY2UyOTBhY2FiMmQyZTlmZDUzOTdiMDgxMzAzNTEwMDBkYzAwODBhZGM0ODExOTI5ZWIwNzBkNGYyMDYzYzg1ODA1ZGI0MThjOTIwZThjMjdlMWUzZGJiZTZkMzZmZmE1MDY4MGYzNDMxNjkyNGU1NWIyMTJmODRkNzdiMDVjZDliMjFkNjMwMzNiYzU5ZDgxNmQ1YTMwYjAyYzY2NjMzZGZmZWVlOTg4ODVkNzhlYmM5MzUyZjIzMWE3YmI3ZTNlNDIwNjZjMjIzYWM1MzIxZjIzM2VlZjJjN2U4OGQ1MGRlOThhZjJhOTIxODI4MDM5MjEyZTNjNTc2OGZlNTNlOTUyMGQxOTc5OThiYmMwZWNhNjU2Y2RjMTBkNzYzZmE2NDliOGY1Yjk3MTZkMDUzYjg4YTQxNjUxMTJkNmQzZDg3ZWIzOGYyN2ZlZWEwOGUwNDM4OTNiNjAxNDI4MjU2ZDc3ZjUzMjQ0YzE2ZmY2MDY0MDE4N2E2MDlkNmJiN2IwZDRkZmM1ZTRmMzNiZTg5MDNlNDc5MjA2NzVjZDE1NzlmMWIyMjJkN2ZjZDEwYzlmMzcwN2FkMGIyYTc2NjM5ZDNlNmRlMmQ2ODAyNmQ4Mzg0OTM0MGZhMTZmZWEzZDFmOTA1NGRkODRhOTQyOTI3NWRmOWM2MmM4MDU2OTAyMTQwNzBiYjA5ODIxOTM1MTRjOGVhZjEwMmIwODY0ZmYwMGU3NzhkZWE1NGNlYjNjMDExZDU0ZDhiN2FkN2NiZjhlMzc5NGY0ZDBmMWUxMWFiNTViY2MxZjIwNGQzYzc5ZGQyMGFhNTMwZjk1YTM1MWRlOGYyY2MxM2NmMzgxNjRiOWZkIn0sInNlY3VyaXR5VG9rZW4iOnsiaXYiOiJiYmFmMjM0NDA5MDRlYmZhMDk2YWJkNzc4ZTYyNGFmOSIsImNvbnRlbnQiOiJlZTUwZGE2NTNhYzliYzY1ZTgwNmFlNTM2OWRlY2Y5MWNjZGI2MDgyOWY4YjE3MzkxZjM1YTA0NzhkYjc4NGY0MjMzZGY1MTQ1NzZlMGE0NDk5NmYwY2U1In0sImp0aSI6IjZhMWI2ZWM5LTdkYzYtNGQ3Zi05MTk1LWRhM2Y3ZDIyMTEwMiIsImlhdCI6MTYzNjg2NzkxNSwiZXhwIjoxNjM2ODcxNTE1fQ.Z33YgxyrXaA54s0Q0oilvQDL6t0dJZULcUIKeiRHzls"
-var urlincident = "https://api.iq.inrix.com/v1/incidents?box=37.81154996811045%7C-122.53116454369045%2C37.709691538514235%7C-122.34959352631057&incidentoutputfields=All&incidenttype=Incidents&locale=en"
+var token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6Imp6eDV4NzFtYTQiLCJ0b2tlbiI6eyJpdiI6ImQ4MmQwMjhmMzM0OGE3NjQ2ZjdmZjNmNWU0MzVhYjI3IiwiY29udGVudCI6Ijc3YzhkNjVmNjM0ZmExMmMxNmU2M2MxMzc0MjQ5NjYyNmY0MmNlN2VkY2E5NDkzNDA4ZGZkOWQ2MjE3MDdlOTgzZWRhYzU2ZTc3ZGIwYWVmNDExNjE2YWI4YTU2MWQ4YjA3NDIwNWQxNWE5N2ZlZGE3Y2Q2ZmUxYzJlOTgxYTI3YWY1OTFkNGJhMTUyNTc3MGNjNTE5YjdhY2MxMDg5MGFmMzdlMmJmMTljNWNkODhhYWRhM2UzYTgzZDhkZGY5YTdmNTExNGQwMDJjYTZjOTFkYWYwMTE3MDVjYjIyOWFjY2RkYWIyOGYxNTVlMzc3ZWVkN2E3ODMwNGZlZWE4MzEwODcyZGI0MWUwMTJjYjAxZGQ1NzJiOWQxZTg3Mzk0MzI2M2E5NDU0NDYxNTU0OWVjZjhlNDI5MDViOWIzMjI0MGUxNmVmN2IwZDU0YTQwN2Y1OWQ4OTZkOTBmN2M2MDc4MzQyOWRjMTU5ODQyYTljNDZkMWRmODYxNzI0ODliMGIxMWNhM2YwMTBlYzY5MjhkNjdjNDc5ODYwNTA1YjQzN2Q3OWE3ZDVmMGU5ZGYzMDg5YTM1YWMxMjQ3NmFjNmZjNjBjNTBmZWUxNTQ2ZGI1MjlmZDA0YWY3OGZmYzNiOWMwYjhkNWMwZGNjNzAyZDcyNmY5ZmNjMjE3YzkzMzgxY2YzMWMxODRiZWEyNmFkYTcyYjVjMGQ3YWU2MTBjNjllOWJiYWU3ZjE3ZWFkNDNjNDc3ZmQ1MGU0OTc2M2ZhYjU5NTRjMjViZTZlNGM0N2ExMTY0MGUzZDJmYjc4ZGZmY2Y0YjY2MjU0M2YxMzQ4M2FiMGRlN2Y0ZjgwODMwYTRjNjU2OGM1ODFmMzlmNThkNDFhZWU4In0sInNlY3VyaXR5VG9rZW4iOnsiaXYiOiJkODJkMDI4ZjMzNDhhNzY0NmY3ZmYzZjVlNDM1YWIyNyIsImNvbnRlbnQiOiI3YWRlYzg0MjQ1M2Q4MzExNmFjMTNmMjgxNzM0YjYzMTE5NThhZDRjZDA4NDI5NmUyOGQyZDZhNDNhMmQ0NWY4MmVmZWJiMTIwM2YwMjJlYTFhNDIzZDk1In0sImp0aSI6ImRlNWRjNTAzLWFlZmUtNDY2MS1hMzJiLTA4YjQ0ZGY3ZjczNCIsImlhdCI6MTYzNjg3MzQ5NSwiZXhwIjoxNjM2ODc3MDk1fQ.sExo36GJOa3GCR3Umf9INA4C-hE6ZmIkvSk8k4EId5E"
+var urlincident = "https://api.iq.inrix.com/v1/incidents?box=37.757387%7C-122.490667%2C37.746138%7C-122.395481&incidentoutputfields=All&incidenttype=Incidents,Flow,Construction&locale=en"
 var urlroute = "https://api.iq.inrix.com/findRoute?wp_1=37.771305%2C-122.424255&wp_2=37.778340%2C-122.423349&maxAlternates=2&routeOutputFields=p&format=json"
 var urlslow = "https://api.iq.inrix.com/v1/dangerousSlowdowns?box=37.81154996811045%7C-122.53116454369045%2C37.709691538514235%7C-122.34959352631057&units=1"
 
@@ -58,6 +58,11 @@ for (let i = 0; i < Dangslow.result.dangerousSlowdowns.length; ++i) {
     sevArray.push(Dangslow.result.dangerousSlowdowns[i].severity);
 }
 
+var dummyIncident = [-122.42241382598877, 37.77271270751953];
+var dummySeverity = 3;
+totArray.push(dummyIncident);
+sevArray.push(dummySeverity);
+
 function incident_on_route() {
     var incCounter = [itemRoute.result.trip.routes.length];
     for (let init = 0; init < itemRoute.result.trip.routes.length; ++init) {
@@ -70,7 +75,7 @@ function incident_on_route() {
         for(let j = 0; j < totArray.length; ++j) 
         {    
             for(let k = 0; k < itemRoute.result.trip.routes[i].points.coordinates.length; ++k)
-            { 
+            {   
                 //console.log("Difference: ", Math.abs(incArray[j].geometry.coordinates[0]-itemRoute.result.trip.routes[i].points.coordinates[k][0]));
                 // Comparing the Coordinates of the Incident and the points on the route
                 if((Math.abs(itemRoute.result.trip.routes[i].points.coordinates[k][1] - totArray[j][1])) <= 0.001 
@@ -89,7 +94,7 @@ function incident_on_route() {
 var incidents = incident_on_route();
 console.log(incidents);
 console.log(itemInc.result.incidents.length);
-
+console.log(itemRoute.result.trip.routes[0].points.coordinates.length);
 
 function avoidance() {
     for (let j = 0; j < incArray.length; ++j) {
@@ -107,3 +112,7 @@ function avoidance() {
 }
 
 //avoidance();
+
+for (let x = 0; x < itemRoute.result.trip.routes[0].points.coordinates.length; ++x) {
+    console.log(itemRoute.result.trip.routes[0].points.coordinates[x]);
+}
